75e8ae4eb1c290b43a502db00b3ef249819bcd05
Frankam487 <fkamgang06@gmail.com>
2024-10-18 08:55:07 +0100

    fist commit

---
+++ app.js
@@ -0,0 +1,35 @@
+let currentInput = ''; 
+
+function clearDisplay() {
+    currentInput = '';
+    document.getElementById("display").value = currentInput;
+}
+
+
+const appendToDisplay = (value) => {
+    currentInput += value;
+    document.getElementById("display").value = currentInput;
+}
+
+const calculateResult = () => {
+    try {
+        
+        let result = eval(currentInput);
+
+        if (!isFinite(result)) {
+            result = "Erreur";
+        }
+
+        currentInput = result.toString();
+        document.getElementById("display").value = currentInput;
+    } catch (error) {
+       
+        document.getElementById("display").value = "Erreur";
+        currentInput = '';
+    }
+}
+
+function deleteLast() {
+    currentInput = currentInput.slice(0, -1);
+    document.getElementById("display").value = currentInput;
+}
