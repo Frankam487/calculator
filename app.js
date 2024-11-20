32e3fe86082bb01e266b5d2c4620b2cbdfdb6fd8
Frank kamgang <143176710+Frankam487@users.noreply.github.com>
2024-11-21 12:44:31 +1400

    Update app.js

--- app.js
+++ app.js
@@ -8,25 +8,19 @@ const appendToDisplay = (value) => {
     currentInput += value;
     document.getElementById("display").value = currentInput;
 }
-
 const calculateResult = () => {
-    try {
-        
+    try {       
         let result = eval(currentInput);
-
         if (!isFinite(result)) {
             result = "Erreur";
         }
-
         currentInput = result.toString();
         document.getElementById("display").value = currentInput;
     } catch (error) {
-       
-        document.getElementById("display").value = "Erreur";
+       document.getElementById("display").value = "Erreur";
         currentInput = '';
     }
 }
-
 function deleteLast() {
     currentInput = currentInput.slice(0, -1);
     document.getElementById("display").value = currentInput;
